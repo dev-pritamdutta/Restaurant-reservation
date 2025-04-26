6a65a76e3d14540c35082be324da7cd7f796d0ef
pritam dutta <webdevpritam7498@gmail.com>
2025-04-27 00:26:03 +0600

    successfully add and get the list data as a admin

---
+++ backend/controllers/productController.js
@@ -0,0 +1,52 @@
+import productModel from "../models/productModels.js";
+import {v2 as cloudinary } from "cloudinary";
+
+const addProduct = async (req, res) => {
+  try {
+    const { name, category, price, description } = req.body;
+    const image = req.file;
+    let imageUrl = "";
+    if (image) {
+      let result = await cloudinary.uploader.upload(image.path, {
+        resouce_type: "image",
+      });
+      imageUrl = result.secure_url;
+    } else {
+      imageUrl = "https://via.placeholder.com/150";
+    }
+
+    const productData = {
+      name,
+      category,
+      price: Number(price),
+      description,
+      image: imageUrl,
+      date: Date.now(),
+    };
+    console.log(productData);
+    const product = new productModel(productData);
+    await product.save();
+
+    res.json({ success: true, message: "Product added successfully", product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Can't added product" });
+  }
+};
+
+const listProducts = async (req, res) => {
+
+    try {
+        const products = await productModel.find({});
+        res.json({ success: true, products });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Can't get products" });
+    }
+};
+
+const removeProduct = async (req, res) => {};
+
+const singelProduct = async (req, res) => {};
+
+export { addProduct, listProducts, removeProduct, singelProduct };
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(cors());
 app.use(express.json())
 
 app.use('/api/user', userRouter)
+app.use("/api/product", productRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
